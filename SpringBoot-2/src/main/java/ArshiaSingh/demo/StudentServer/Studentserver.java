7457ee202d71166f7515ba4ff7b6bad7bfef79fd
ArshiaSingh05 <singharshia2005@gmail.com>
2026-07-14 12:47:46 +0530

    Postman

--- SpringBoot-2/src/main/java/ArshiaSingh/demo/StudentServer/Studentserver.java
+++ SpringBoot-2/src/main/java/ArshiaSingh/demo/StudentServer/Studentserver.java
@@ -1,10 +1,21 @@
 package ArshiaSingh.demo.StudentServer;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Studentserver {
     // 1. Store the student
+    @PostMapping("/create")
+    public String storeStudent(RequestBody ){
+        return """
+                id: 1
+                name: Arshia
+                Department: CSE
+                age: 20
+                """;
+    }
 
     // 2. Read the student with id
     // 3. Update student info
